4c3777607d4ddb2370b151caa6a7699ea2d07e4b
Arush Bansal <arush4bansal@gmail.com>
2023-09-20 20:00:46 +0530

    404 page

--- src/pages/error404.js
+++ src/pages/error404.js
@@ -6,11 +6,14 @@ export default function Error404() {
   const navigate = useNavigate();
 
   return (
-    <Container maxWidth="sm" sx={{ py: 2 }}>
+    <Container maxWidth="md" sx={{ py: 2 }}>
       <Card>
-        <CardHeader title={<Typography variant="h4" color="error.light">Error 404</Typography>} subheader="Page not found" />
+        <CardHeader 
+          title={<Typography variant="h4" color="error.light">404 Not Found</Typography>} 
+          subheader={<Typography sx={{mt:2}}>Page not found. Please check if the URL is correct. </Typography>}
+        />
         <CardActions sx={{ ml: 1, mb: 1 }}>
-          <Button variant="contained" onClick={() => { navigate('/') }}><Typography>Home</Typography></Button>
+          <Button variant="contained" onClick={() => { navigate('/') }}><Typography >Go Home</Typography></Button>
         </CardActions>
       </Card>
     </Container>
