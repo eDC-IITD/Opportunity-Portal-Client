f5045033a2ba4d43fa56d8e4cd2d27c9791132ce
Arush Bansal <arush4bansal@gmail.com>
2023-10-05 10:09:25 +0530

    admin integration start

--- src/pages/admin/signIn.js
+++ src/pages/admin/signIn.js
@@ -1,31 +1,27 @@
 import { Card, CardContent, CardHeader, Container, Typography, TextField, CardActions, Button, CircularProgress } from '@mui/material';
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom';
 
-export default function SignIn({ BASE_URL, setShowAlert, setAlertMessage, setAlertSeverity }) {
+export default function AdminSignIn({ BASE_URL, setShowAlert, setAlertMessage, setAlertSeverity }) {
   const { user } = useLocation().state;
   const [email, setEmail] = useState('');
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  const loginStudent = async (e) => {
+  const loginAdmin = async (e) => {
     e.preventDefault();
     setLoading(true);
     if (email.substring(email.length - 10, email.length) !== "iitd.ac.in") {
-      setAlertMessage("Please enter IIT Delhi email ID.");
+      setAlertMessage("Please enter authorized email only");
       setAlertSeverity("info");
       setShowAlert(true);
       setLoading(false);
       return
     }
-    const formData = {
-      email: email,
-    }
+    const formData = {email: email,}
     const requestOptions = {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: {"Content-Type": "application/json",},
       body: JSON.stringify(formData)
     }
     const url = `${BASE_URL}/api/student/login`;
@@ -52,45 +48,7 @@ export default function SignIn({ BASE_URL, setShowAlert, setAlertMessage, setAle
       console.log(error)
     }
   }
-
-  const loginStartUp = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    const formData = {
-      email: email,
-    }
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData)
-    }
-    const url = `${BASE_URL}/api/startUp/login`;
-    try {
-      await fetch(url, requestOptions)
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.status === 200) {
-            setLoading(false);
-            navigate('../otpVerify', { state: { user: "Startup", signInOrSignUp: "SignIn", email: data.startUpDetails.email, name: data.startUpDetails.companyName } });
-          }
-          else if (data.status === 401) {
-            setLoading(false);
-            setAlertMessage("Account doesn't exist. Please signup.");
-            setAlertSeverity("info");
-            setShowAlert(true);
-          }
-          else {
-            console.log(data);
-          }
-        })
-    }
-    catch (error) {
-      console.log(error)
-    }
-  }
-
+  
   return (
     <Container maxWidth="sm" sx={{ py: 2, mt: 9 }}>
       <form onSubmit={user === "Student" ? loginStudent : loginStartUp}>
