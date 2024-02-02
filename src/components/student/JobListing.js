2e914b95fdc17016ed64b66ad9139a97a178756e
spurjhattam2207 <133047073+spurjhattam2207@users.noreply.github.com>
2024-02-03 00:03:20 +0530

    Done

--- src/components/student/JobListing.js
+++ src/components/student/JobListing.js
@@ -2,12 +2,83 @@ import React from "react";
 import { useMediaQuery,Box, Typography, Button, Grid, Card, CardContent, CardMedia } from "@mui/material";
 
 
-function JobListing({ logo, companyName, mission, role, salary, deadline, type, detailsButtonClick, applyButtonClick }) {
+function JobListing({ logo, companyName, mission, role, salary, deadline, type, detailsButtonClick, applyButtonClick, status }) {
   const isMobile = useMediaQuery((theme) => theme.breakpoints.down("md"));
+
+  const renderActionButton = () => {
+    if (status === 'Applied' ) {
+      return (
+        <Button
+          sx={{ border: "1px solid #00FFD1", color:"#fff", padding: "1.2vh 3vw", "@media (max-width: 768px)":{paddingBlock :"1.5 vh"}, cursor: "default",backgroundColor: '#888' , '&:hover': {
+            backgroundColor: '#888', 
+          },}}
+        >
+          Applied
+        </Button>
+      );
+    } 
+    else if(status === 'Selected'){
+      return (
+        <Button
+          onClick={detailsButtonClick}
+          sx={{ border: "1px solid #00FFD1", color:"#fff", padding: "1.2vh 2.6vw", "@media (max-width: 768px)":{paddingBlock :"1.5 vh"}, cursor: "default",backgroundColor: '#888' , '&:hover': {
+            backgroundColor: '#4CAF50', 
+          },}}
+        >
+          Selected
+        </Button>
+      );
+    }
+    else if(status === 'Not selected'){
+      return (
+        <Button
+          onClick={detailsButtonClick}
+          sx={{ border: "1px solid #00FFD1", color:"#fff", padding: "1.2vh 1.5vw", "@media (max-width: 768px)":{paddingBlock :"1.5 vh"}, cursor: "default",backgroundColor: '#888' , '&:hover': {
+            backgroundColor: '#F44336', 
+          },}}
+        >
+          Not Selected
+        </Button>
+      );
+    }
+    else if(status === 'Shortlisted'){
+      return (
+        <Button
+          sx={{ border: "1px solid #00FFD1", color:"#fff", padding: "1.2vh 1.7vw", "@media (max-width: 768px)":{paddingBlock :"0.8 vh"}, cursor: "default",backgroundColor: '#888' , '&:hover': {
+            backgroundColor: '#4CAF50', 
+          },}}
+        >
+          Shortlisted
+        </Button>
+      );
+    }
+    else if(status === 'Not shortlisted'){
+      return (
+        <Button
+        sx={{ border: "1px solid #00FFD1", color:"#fff", padding: "1.2vh 0.6vw", "@media (max-width: 768px)":{paddingBlock :"0.8 vh"}, cursor: "default",backgroundColor: '#888' , '&:hover': {
+          backgroundColor: '#F44336', 
+        },}}
+        >
+          Not Shortlisted
+        </Button>
+      );
+    }
+    else {
+      return (
+        <Button
+          onClick={applyButtonClick}
+          sx={{ borderRadius: "6px", backgroundColor: "#00FFD1", padding: "1.2vh 2.2vw", color: "black", "@media (max-width: 768px)": { paddingBlock: "1.1vh" } }}
+        >
+          Apply Now
+        </Button>
+      );
+    }
+  };
+
   return (
     <Card sx={{ borderRadius: "20px", boxShadow: "0px 3px 20px 0px rgba(0, 0, 0, 0.25)" }}>
       <Grid container alignItems="center" justifyContent="space-between" gap={2} sx={{ paddingBlock:"1vh", paddingInline:"2vw", "@media (max-width: 768px)": { gap:"0" } }}>
-      {!isMobile && (
+        {!isMobile && (
           <Grid item>
             <CardMedia component="img" src={logo} loading="lazy" alt="Company Logo" sx={{ aspectRatio: "0.94", objectFit: "contain", objectPosition: "center", width: "7vw", overflow: "hidden" }} />
           </Grid>
@@ -16,7 +87,7 @@ function JobListing({ logo, companyName, mission, role, salary, deadline, type, 
           <CardContent sx={{ color: "#fff"}}>
             <Typography variant="h6" component="div" fontWeight="bold">{companyName}</Typography>
             {!isMobile && (
-            <Typography variant="body1" sx={{ color: "#d0cccc" }}>Our mission is to create a world where everyone has access to the food they love{mission}</Typography>
+              <Typography variant="body1" sx={{ color: "#d0cccc" }}>Our mission is to create a world where everyone has access to the food they love{mission}</Typography>
             )}
             <Grid container alignItems="flex-start" justifyContent="space-between" gap={1} >
               <Grid item>
@@ -36,10 +107,8 @@ function JobListing({ logo, companyName, mission, role, salary, deadline, type, 
         </Grid>
         <Grid item sx={{margin:"auto"}}>
           <Grid container flexDirection="column" gap={1} alignItems="center" sx={{"@media (max-width: 768px)":{flexDirection:"row"}}}>
-            <Grid item sx={{margin:"auto"}}>
-              <Button onClick={applyButtonClick}   sx={{  borderRadius: "6px", backgroundColor:"#00FFD1;",padding: "1vh 2.2vw", color:"black",  "@media (max-width: 768px)":{paddingBlock :"1.1vh"} }}>
-                Apply Now
-              </Button>
+            <Grid item sx={{ margin: "auto" }}>
+              {renderActionButton()}
             </Grid>
             <Grid item>
               <Button onClick={detailsButtonClick}  sx={{ border: "1px solid #00FFD1", color:"#00FFD1", padding: "1vh 3vw", "@media (max-width: 768px)":{paddingBlock :"1.5 vh"} }}>
@@ -52,5 +121,6 @@ function JobListing({ logo, companyName, mission, role, salary, deadline, type, 
     </Card>
   );
 }
+
 
 export default JobListing;
